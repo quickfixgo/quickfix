91fd7e598fedbec83f7d56371c81adae1d823688
Chris Busbey <cbusbey@connamara.com>
2016-02-15 10:16:44 -0800

    typo

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package quickfix is a full featured messaging engine for the FIX protocol.  It is a 100% Go open source implementaiton of the popular C++ QuickFIX engine (http://quickfixengine.org).
+Package quickfix is a full featured messaging engine for the FIX protocol.  It is a 100% Go open source implementation of the popular C++ QuickFIX engine (http://quickfixengine.org).
 
 Creating your QuickFIX Application
 
