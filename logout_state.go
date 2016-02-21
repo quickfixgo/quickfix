d832f50f07b558724cd3c5f37ff831c49ed2b5bb
Chris Busbey <cbusbey@connamara.com>
2016-02-21 11:20:39 -0800

    remove session from public api

--- logout_state.go
+++ logout_state.go
@@ -3,11 +3,11 @@ package quickfix
 type logoutState struct {
 }
 
-func (state logoutState) FixMsgIn(session *Session, msg Message) (nextState sessionState) {
+func (state logoutState) FixMsgIn(session *session, msg Message) (nextState sessionState) {
 	return state
 }
 
-func (state logoutState) Timeout(session *Session, event event) (nextState sessionState) {
+func (state logoutState) Timeout(session *session, event event) (nextState sessionState) {
 	switch event {
 	case logoutTimeout:
 		session.log.OnEvent("Timed out waiting for Logout response")
