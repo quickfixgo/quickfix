b88d26b130203a0f18764c5d0297b9676aac8569
Isaac Andrade <isaac.nic@gmail.com>
2016-03-01 11:57:23 -0700

    Regenerate fix types. - This is a follow-up of last commit where a typo was fixed in the   _gen/generate-messages subprogram.

--- fix42/news/News.go
+++ fix42/news/News.go
@@ -103,7 +103,7 @@ func (m Message) Marshal() quickfix.Message { return quickfix.Marshal(m) }
 //A RouteOut is the callback type that should be implemented for routing Message
 type RouteOut func(msg Message, sessionID quickfix.SessionID) quickfix.MessageRejectError
 
-//Route returns the beginstring, message type, and MessageRoute for this Mesage type
+//Route returns the beginstring, message type, and MessageRoute for this Message type
 func Route(router RouteOut) (string, string, quickfix.MessageRoute) {
 	r := func(msg quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
 		m := new(Message)
