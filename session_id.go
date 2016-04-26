327f8b45658244c9df117937c1eb21b8782fdaa7
Brad Haan <bhaan@connamara.com>
2016-04-26 10:47:03 -0500

    validator interface (#120)
    * validator interface for fix and fixt sessions
    
    * godoc comment for SessionID IsFXIT()

--- session_id.go
+++ session_id.go
@@ -2,11 +2,17 @@ package quickfix
 
 import (
 	"fmt"
+	"github.com/quickfixgo/quickfix/enum"
 )
 
 // SessionID is a unique identifer of a Session
 type SessionID struct {
 	BeginString, TargetCompID, TargetSubID, TargetLocationID, SenderCompID, SenderSubID, SenderLocationID, Qualifier string
+}
+
+//IsFIXT returns true if the SessionID has a FIXT BeginString
+func (s SessionID) IsFIXT() bool {
+	return s.BeginString == enum.BeginStringFIXT11
 }
 
 func (s SessionID) String() string {
