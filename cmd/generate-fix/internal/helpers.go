1b43940d5626673867e1f8d61312a2ef6fdb074e
ackleymi <ackleymi@gmail.com>
2019-10-17 22:40:53 -0400

    Adds a flag to specify generated package import paths.

--- cmd/generate-fix/internal/helpers.go
+++ cmd/generate-fix/internal/helpers.go
@@ -1,18 +1,6 @@
 package internal
 
 // getImportPathRoot returns the root path to use in import statements.
-// The root path is determined by stripping "$GOPATH/src/" from the current
-// working directory.  For example, when generating code within the QuickFIX/Go
-// source tree, the returned root path will be "github.com/quickfixgo/quickfix".
 func getImportPathRoot() string {
-	// pwd, err := os.Getwd()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// goSrcPath := filepath.Join(os.Getenv("GOPATH"), "src")
-	// importPathRoot := filepath.ToSlash(strings.Replace(pwd, goSrcPath, "", 1))
-	// return strings.TrimLeft(importPathRoot, "/")
-
-	// !! A hack for now? .....
-	return "github.com/quickfixgo/quickfix/gen"
+	return *pkgRoot
 }
