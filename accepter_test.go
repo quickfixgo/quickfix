b24fac80032f65288f43fd133b803aabc0d2ab62
Michael L. Wilner <mike@wilner.me>
2021-07-01 13:17:56 -0500

    Test for acceptors per session

--- accepter_test.go
+++ accepter_test.go
@@ -4,11 +4,18 @@ import (
 	"net"
 	"testing"
 
-	"github.com/armon/go-proxyproto"
+	"github.com/quickfixgo/quickfix/config"
+
+	proxyproto "github.com/armon/go-proxyproto"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAcceptor_Start(t *testing.T) {
+	sessionSettings := NewSessionSettings()
+	sessionSettings.Set(config.BeginString, BeginStringFIX42)
+	sessionSettings.Set(config.SenderCompID, "sender")
+	sessionSettings.Set(config.TargetCompID, "target")
+
 	settingsWithTCPProxy := NewSettings()
 	settingsWithTCPProxy.GlobalSettings().Set("UseTCPProxy", "Y")
 
@@ -23,8 +30,8 @@ func TestAcceptor_Start(t *testing.T) {
 	)
 
 	acceptorStartTests := []struct {
-		name string
-		settings *Settings
+		name         string
+		settings     *Settings
 		listenerType int
 	}{
 		{"with TCP proxy set", settingsWithTCPProxy, ProxyListener},
@@ -35,19 +42,26 @@ func TestAcceptor_Start(t *testing.T) {
 	for _, tt := range acceptorStartTests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.settings.GlobalSettings().Set("SocketAcceptPort", "5001")
+			if _, err := tt.settings.AddSession(sessionSettings); err != nil {
+				assert.Nil(t, err)
+			}
 
 			acceptor := &Acceptor{settings: tt.settings}
 			if err := acceptor.Start(); err != nil {
 				assert.NotNil(t, err)
 			}
-			if tt.listenerType == ProxyListener {
-				_, ok := acceptor.listener.(*proxyproto.Listener)
-				assert.True(t, ok)
-			}
+			assert.Len(t, acceptor.listeners, 1)
 
-			if tt.listenerType == GenericListener {
-				_, ok := acceptor.listener.(*net.TCPListener)
-				assert.True(t, ok)
+			for _, listener := range acceptor.listeners {
+				if tt.listenerType == ProxyListener {
+					_, ok := listener.(*proxyproto.Listener)
+					assert.True(t, ok)
+				}
+
+				if tt.listenerType == GenericListener {
+					_, ok := listener.(*net.TCPListener)
+					assert.True(t, ok)
+				}
 			}
 
 			acceptor.Stop()
