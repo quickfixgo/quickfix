e615c97343e0d85917496ec869e28ad38edfb6e8
xuchao <xuchao.chi@longbridge.sg>
2023-02-22 02:01:45 +0800

    del messages createtime

--- gormstore_model.go
+++ gormstore_model.go
@@ -23,17 +23,16 @@ func (g GormSessions) TableName() string {
 }
 
 type GormMessages struct {
-	BeginString      string    `gorm:"column:beginstring;primaryKey;type:varchar(8)"`
-	SenderCompId     string    `gorm:"column:sendercompid;primaryKey;type:varchar(64)"`
-	SenderSubId      string    `gorm:"column:sendersubid;primaryKey;type:varchar(64)"`
-	SenderLocId      string    `gorm:"column:senderlocid;primaryKey;type:varchar(64)"`
-	TargetCompId     string    `gorm:"column:targetcompid;primaryKey;type:varchar(64)"`
-	TargetSubId      string    `gorm:"column:targetsubid;primaryKey;type:varchar(64)"`
-	TargetLocId      string    `gorm:"column:targetlocid;primaryKey;type:varchar(64)"`
-	SessionQualifier string    `gorm:"column:session_qualifier;primaryKey;type:varchar(64)"`
-	CreatedAt        time.Time `gorm:"column:created_at"`
-	Message          string    `gorm:"column:message;type:text"`
-	MsgSeqNum        int64     `gorm:"column:msgseqnum;primaryKey"`
+	BeginString      string `gorm:"column:beginstring;primaryKey;type:varchar(8)"`
+	SenderCompId     string `gorm:"column:sendercompid;primaryKey;type:varchar(64)"`
+	SenderSubId      string `gorm:"column:sendersubid;primaryKey;type:varchar(64)"`
+	SenderLocId      string `gorm:"column:senderlocid;primaryKey;type:varchar(64)"`
+	TargetCompId     string `gorm:"column:targetcompid;primaryKey;type:varchar(64)"`
+	TargetSubId      string `gorm:"column:targetsubid;primaryKey;type:varchar(64)"`
+	TargetLocId      string `gorm:"column:targetlocid;primaryKey;type:varchar(64)"`
+	SessionQualifier string `gorm:"column:session_qualifier;primaryKey;type:varchar(64)"`
+	Message          string `gorm:"column:message;type:text"`
+	MsgSeqNum        int64  `gorm:"column:msgseqnum;primaryKey"`
 }
 
 func (g GormMessages) TableName() string {
