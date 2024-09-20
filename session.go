393a748fdfe2083e45c261301566e51501216155
Michael Ackley <mackley@connamara.com>
2024-09-20 15:30:43 -0500

    Reverts toadmin call sequencing

--- session.go
+++ session.go
@@ -356,6 +356,7 @@ func (s *session) prepMessageForSend(msg *Message, inReplyTo *Message) (msgBytes
 	}
 
 	if isAdminMessageType(msgType) {
+		s.application.ToAdmin(msg, s.sessionID)
 		if bytes.Equal(msgType, msgTypeLogon) {
 			var resetSeqNumFlag FIXBoolean
 			if msg.Body.Has(tagResetSeqNumFlag) {
@@ -374,7 +375,6 @@ func (s *session) prepMessageForSend(msg *Message, inReplyTo *Message) (msgBytes
 				msg.Header.SetField(tagMsgSeqNum, FIXInt(seqNum))
 			}
 		}
-		s.application.ToAdmin(msg, s.sessionID)
 	} else {
 		if err = s.application.ToApp(msg, s.sessionID); err != nil {
 			return
